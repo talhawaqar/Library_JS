37be9971cf780e7339912c63e98d3854d29c6621
lamiasristy <lamiasristy@gmail.com>
2020-12-04 18:23:48 +0100

    Fixed the Linters errors.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const read = document.getElementById('read');
 const displayFromBtn = document.getElementById('new-bookbtn');
 const form = document.getElementById('create-form');
 
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, isread) {
   this.title = title;
@@ -30,93 +30,88 @@ addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
 
-function createIsReadBtn(book){
-    const readBtn = document.createElement('button');    
-     // Check for read or not
-     if(book.isread) {
-         readBtn.textContent = 'Read';
-         readBtn.classList.toggle('readbtn');
-     }    
-     else{
-        readBtn.textContent = 'NotRead';
-        readBtn.classList.toggle('notreadbtn');
-     } 
-     //toggle btn for isRead
-     readBtn.addEventListener('click', function() {
-         book.isread=!book.isread;
-         readBtn.textContent = book.isread? 'Read' : 'NotRead';    
-         readBtn.classList.toggle('readbtn');
-         readBtn.classList.toggle('notreadbtn');   
-     });
-    return readBtn; 
+function createIsReadBtn(book) {
+  const readBtn = document.createElement('button');
+  // Check for read or not
+  if (book.isread) {
+    readBtn.textContent = 'Read';
+    readBtn.classList.toggle('readbtn');
+  } else {
+    readBtn.textContent = 'NotRead';
+    readBtn.classList.toggle('notreadbtn');
+  }
+  // toggle btn for isRead
+  readBtn.addEventListener('click', () => {
+    book.isread = !book.isread;
+    readBtn.textContent = book.isread ? 'Read' : 'NotRead';
+    readBtn.classList.toggle('readbtn');
+    readBtn.classList.toggle('notreadbtn');
+  });
+  return readBtn;
 }
 
-function createDeleteBtn(index){
+function displayLibrary(array) {
+  container.innerHTML = '';
+
+  for (let i = 0; i < array.length; i += 1) {
+    const book = array[i];
+
+    // Create the element
+    const div = document.createElement('div');
+    div.classList.add('bookdiv'); // Add the style for div
+
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+
+    // Data insertion
+    title.textContent = book.title;
+    author.textContent = `Author: ${book.author}`;
+    pages.textContent = `No.of pages: ${book.pages}`;
+
+    // delete a book Btn
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('deletebtn');
-    //delete a book Btn
-    deleteBtn.textContent = 'Delete';    
 
-    deleteBtn.addEventListener('click', function() {
-        alert(`Are you sure? You want to delete this book?`);
-        myLibrary.splice(index, 1);
-        displayLibrary(myLibrary);
+    deleteBtn.textContent = 'Delete';
+
+    deleteBtn.addEventListener('click', () => {
+      alert('Are you sure? You want to delete this book?');
+      myLibrary.splice(i, 1);
+      displayLibrary(myLibrary);
     });
 
-    return deleteBtn;
-}
-
-function displayLibrary(array){
-    container.innerHTML = '';
-
-    for (let i = 0; i < array.length; i += 1) {
-
-        const book = array[i];
-
-        //Create the element
-        const div = document.createElement('div');
-        div.classList.add('bookdiv'); // Add the style for div
-        
-        const title_El = document.createElement('h3');
-        const author_El = document.createElement('p');
-        const pages_El = document.createElement('p');  
-        
-        // Data insertion
-        title_El.textContent = book.title;        
-        author_El.textContent = `Author: ${book.author}`;
-        pages_El.textContent = `No.of pages: ${book.pages}`;
-
-        //Organogram
-        container.appendChild(div);
-        div.appendChild(title_El);
-        div.appendChild(author_El);
-        div.appendChild(pages_El);        
-        div.appendChild(createIsReadBtn(book));
-        div.appendChild(createDeleteBtn(i));
-    }
+    // Organogram
+    container.appendChild(div);
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(createIsReadBtn(book));
+    div.appendChild(deleteBtn);
+  }
 }
 
 displayLibrary(myLibrary);
 
 // Display the form
-displayFromBtn.addEventListener('click', function() {
-    form.classList.toggle('display');
+displayFromBtn.addEventListener('click', () => {
+  form.classList.toggle('display');
 });
 
 // Add a new book
-submitBtn.addEventListener("click", function(e){
-    e.preventDefault(); // This will prevent the default behaviour of the event.
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault(); // This will prevent the default behaviour of the event.
 
-    if(title.value === '' || author.value === ''|| pages.value === ''){
-        alert(`Can't create book because some properties are missing`);
-        return;
-    }
+  if (title.value === '' || author.value === '' || pages.value === '') {
+    alert('Cannot create book because some properties are missing');
+    return;
+  }
 
-    let newBook = new Book(title.value, author.value, pages.value, read.checked);
-    addBookToLibrary(newBook);
-    displayLibrary(myLibrary);
-    title.value = '';
-    author.value = '';
-    pages.value = '';
-    read.checked = false;
+  const newBook = new Book(title.value, author.value, pages.value, read.checked);
+  addBookToLibrary(newBook);
+  displayLibrary(myLibrary);
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
 });
